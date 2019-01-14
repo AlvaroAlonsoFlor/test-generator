06f3854a77933530e878ed01271be55d5284b410
VladmirVolgen <alvaroalonsoflor@gmail.com>
2019-01-14 14:33:16 +0000

    Add handleUpload method to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-import excelReader from './helpers/excel_reader.js'
+import XLSX from 'xlsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    }
+     this.handleUpload = this.handleUpload.bind(this);
+
+  }
+
+  handleUpload(event) {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(event.target.files[0])
+    let sheet;
+
+    reader.onload = () => {
+
+      const data = new Uint8Array(reader.result);
+      const workbook = XLSX.read(data, {type: 'array'});
+      const worksheet_name = workbook.SheetNames[0]
+
+      sheet = workbook.Sheets[worksheet_name];
+      // Transform sheet into readable format
+      sheet = XLSX.utils.sheet_to_json(sheet, { header: ["num", "answer"]});
+
+      this.setState({data: sheet})
+
+      console.log(sheet);
+    }
+  }
   render() {
 
     return (
       <div className="App">
-        App working
+        <input type="file" id="excel-file" onChange={this.handleUpload}/>
       </div>
     );
   }
